3147eb3133d3cf529a221a5aca09649de0d17fa4
crea8ive <crea8ivedev@gmail.com>
2024-09-27 17:19:00 +0530

    feat: announcement bar

---
+++ assets/custom.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".announcement-bar", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".next-arrow",  
+      prevEl: ".prev-arrow",  
+    },
+  });
+  var swiper = new Swiper(".video-banner-swiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".next-arrow",  
+      prevEl: ".prev-arrow",  
+    },
+  });
+  
+    
+      function HideAnnouncementBar() {
+        document.querySelector('.ctm-announcement-bar').style.display = 'none';
+      }
+
